aed1f3fd708c5d14d77db3481e7b451d80319eb6
Draconix_Dust <gabriacciaio@libero.it>
2023-07-11 20:09:38 +0200

    first push

---
+++ src_code.cc
@@ -0,0 +1,177 @@
+#include <iostream>
+
+using namespace std;
+
+//-FUNCTIONS---------------------
+void startMenu();
+void quitMenu();
+void fileOptions();
+void framesOptions();
+//-------------------------------
+
+//-VARIABLES---------------------
+string inputPath;
+string outputPath;
+string fileName;
+int startFrame = 1;
+int endFrame = 250;
+bool singleFrame = true;
+//-------------------------------
+
+int main(){
+
+    startMenu();
+
+    return 0;
+}
+
+void startMenu(){
+    int menuOption;
+
+    system("CLS");
+    cout << "WELCOME TO THE BLENDER RENDERER SCRIPT GENERATOR!" << endl
+    << "-------------------------------------------------"
+    << endl << endl
+    << "1.\tFile Options" << endl
+    << "2.\tFrames Options" << endl
+    << "3.\tRender Engine" << endl
+    << "4.\tSpecify CPU Threads To Use" << endl
+    << "5.\tBuild And Run" << endl
+    << endl
+    << "0.\tQuit Program" << endl
+    << endl;
+
+    cin >> menuOption;
+
+    if(menuOption == 1){
+        fileOptions();
+    }else if(menuOption == 2){
+        framesOptions();
+    }else if(menuOption == 3){
+        
+    }else if(menuOption == 4){
+        
+    }else if(menuOption == 5){
+        
+    }else if(menuOption == 0){
+        quitMenu();
+    }else{
+        cout << "Number not valid, retry";
+        startMenu();
+    }
+    
+}
+
+void quitMenu(){
+    system("CLS");
+    exit(0);
+}
+
+void fileOptions(){
+    int menuOption;
+
+    system("CLS");
+    cout << "WELCOME TO THE BLENDER RENDERER SCRIPT GENERATOR!" << endl
+    << "-------------------------------------------------"
+    << endl
+    << "File Options:"
+    << endl
+    << "1.\tInput Path" << endl
+    << "2.\tOutput Path" << endl
+    << "3.\tOutput File Name (no extension)" << endl
+    << endl
+    << "0.\tBack" << endl
+    << endl
+    << endl
+    << "Input Path: " + inputPath << endl
+    << "Output Path: " + outputPath << endl
+    << "Output File Name: " + fileName << endl << endl;
+    cin >> menuOption;
+
+    if(menuOption == 1){
+        system("CLS");
+        cout << "This is the path of the .blend file (remember .blend extension!)" << endl
+        << "File Path: ";
+        inputPath.clear();
+        cin >> inputPath;
+        inputPath.insert(0, "\"") += "\"";
+        fileOptions();
+    }else if(menuOption == 2){
+        
+        system("CLS");
+        cout << "This is the path were the file will be saved." << endl
+        << "File Path: ";
+        outputPath.clear();
+        cin >> outputPath;
+        outputPath.insert(0, "\"") += "\"";
+        fileOptions();
+        
+    }else if(menuOption == 3){
+        system("CLS");
+        cout << "Set the name of the output file (if you want the frame(s) number(s) use as many # as many digits you want)" << endl
+        << "File Name: ";
+        fileName.clear();
+        cin >> fileName;
+        fileOptions();
+    }else if(menuOption == 0){
+        system("CLS");
+        startMenu();
+    }else{
+        cout << "ERROR: Number not valid. Retry";
+        fileOptions();
+    }
+
+}
+
+void framesOptions(){
+    int menuOption;
+
+    system("CLS");
+    cout << "WELCOME TO THE BLENDER RENDERER SCRIPT GENERATOR!" << endl
+    << "-------------------------------------------------"
+    << endl
+    << "Frames Options:"
+    << endl
+    << endl
+    << "Single Frame Mode: " << boolalpha << singleFrame << noboolalpha << endl
+    << "1.\tChange Mode" << endl;
+    if(singleFrame){
+        cout << "2.\tSet Frame";
+    }else{
+        cout << "2.\tSet Starting Frame" << endl
+        << "3.\tSet Ending Frame";
+    }
+    cout << endl << endl << "0.\tBack" 
+    << endl
+    << endl;
+    if(singleFrame){
+        cout << "Frame: " << startFrame;
+    }else{
+        cout << "Starting Frame: " << startFrame << endl
+        << "Ending Frame: " << endFrame;
+    }
+    cout << endl << endl;
+
+    cin >> menuOption;
+
+    if(menuOption == 1){
+        singleFrame = !singleFrame;
+        framesOptions();
+    }else if(menuOption == 2){
+        system("CLS");
+        cout << "Set Frame: ";
+        cin >> startFrame;
+        framesOptions();
+    }else if(menuOption == 3 && !singleFrame){
+        system("CLS");
+        cout << "Set End Frame: ";
+        cin >> endFrame;
+        framesOptions();
+    }else if(menuOption == 0){
+        system("CLS");
+        startMenu();
+    }else{
+        cout << "ERROR: Number not valid. Retry";
+        framesOptions();
+    }
+}
